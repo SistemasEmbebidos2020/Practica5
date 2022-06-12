0e4437ba57a86b92368b5998757b1846d6dbfa5c
SistemasEmbebidos <78398897+SistemasEmbebidos2020@users.noreply.github.com>
2022-06-12 11:40:45 -0500

    Update readeeprom.c

--- presencial/readeeprom.c
+++ presencial/readeeprom.c
@@ -4,7 +4,7 @@
 #include "keypad.h"
 static unsigned char valor = '0';
 
-void main()
+void readeeproms()
 {
 KEYPAD_init();
 DDRD = 255;
@@ -20,7 +20,7 @@ else if (valor == '6') PORTD = eeprom_read_byte((uint8_t *)(6));
 else if (valor == '7') PORTD = eeprom_read_byte((uint8_t *)(7));
 else if (valor == '8') PORTD = eeprom_read_byte((uint8_t *)(8));
 else if (valor == '9') PORTD = eeprom_read_byte((uint8_t *)(9));
-else if (valor == '0') PORTD = eeprom_read_byte(0);
+else if (valor == '0') PORTD = eeprom_read_byte((uint8_t *)(0));
 else if (valor == 'A') PORTD= eeprom_read_byte((uint8_t *)(10));
 else if (valor == 'B') PORTD = eeprom_read_byte((uint8_t *)(11));
 else if (valor == 'C') PORTD = eeprom_read_byte((uint8_t *)(12));

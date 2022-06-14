f6e75e6501d61dbceb369ee7373cb15e6cbaf51a
SistemasEmbebidos <78398897+SistemasEmbebidos2020@users.noreply.github.com>
2022-06-13 19:31:19 -0500

    Update readeeprom.h

--- presencial/readeeprom.h
+++ presencial/readeeprom.h
@@ -3,31 +3,41 @@
 #include <avr/io.h>
 #include "keypad.h"
 
+#ifndef readeeprom
+#define readeeprom
 void readeeproms()
 {
 KEYPAD_init();
 DDRD = 255;
 char valor = '0';
+char val = 'y';
+int j;
 while(1){
-valor = KEYPAD_read();
-if (valor == '1') PORTD = eeprom_read_byte((uint8_t *)(1));
-else if (valor == '2') PORTD = eeprom_read_byte((uint8_t *)(2));
-else if (valor == '3') PORTD = eeprom_read_byte((uint8_t *)(3));
-else if (valor == '4') PORTD = eeprom_read_byte((uint8_t *)(4));
-else if (valor == '5') PORTD = eeprom_read_byte((uint8_t *)(5));
-else if (valor == '6') PORTD = eeprom_read_byte((uint8_t *)(6));
-else if (valor == '7') PORTD = eeprom_read_byte((uint8_t *)(7));
-else if (valor == '8') PORTD = eeprom_read_byte((uint8_t *)(8));
-else if (valor == '9') PORTD = eeprom_read_byte((uint8_t *)(9));
-else if (valor == '0') PORTD = eeprom_read_byte((uint8_t *)(0));
-else if (valor == 'A') PORTD= eeprom_read_byte((uint8_t *)(10));
-else if (valor == 'B') PORTD = eeprom_read_byte((uint8_t *)(11));
-else if (valor == 'C') PORTD = eeprom_read_byte((uint8_t *)(12));
-else if (valor == 'D') PORTD = eeprom_read_byte((uint8_t *)(13));
-else if (valor == '*') PORTD  = eeprom_read_byte((uint8_t *)(14));
-else if (valor == '#') PORTD  = eeprom_read_byte((uint8_t *)(15));
+
+for (j=1;j<5;j++)
+		{
+		valor = KEYPAD_read();
+		if (valor != val)
+			{
+					if (valor == '1') PORTD = eeprom_read_byte((uint8_t *)(1));
+					else if (valor == '2') PORTD = eeprom_read_byte((uint8_t *)(2));
+					else if (valor == '3') PORTD = eeprom_read_byte((uint8_t *)(3));
+					else if (valor == '4') PORTD = eeprom_read_byte((uint8_t *)(4));
+					else if (valor == '5') PORTD = eeprom_read_byte((uint8_t *)(5));
+					else if (valor == '6') PORTD = eeprom_read_byte((uint8_t *)(6));
+					else if (valor == '7') PORTD = eeprom_read_byte((uint8_t *)(7));
+					else if (valor == '8') PORTD = eeprom_read_byte((uint8_t *)(8));
+					else if (valor == '9') PORTD = eeprom_read_byte((uint8_t *)(9));
+					else if (valor == '0') PORTD = eeprom_read_byte(0);
+					else if (valor == 'A') PORTD= eeprom_read_byte((uint8_t *)(10));
+					else if (valor == 'B') PORTD = eeprom_read_byte((uint8_t *)(11));
+					else if (valor == 'C') PORTD = eeprom_read_byte((uint8_t *)(12));
+					else if (valor == 'D') PORTD = eeprom_read_byte((uint8_t *)(13));
+					else if (valor == '*') PORTD  = eeprom_read_byte((uint8_t *)(14));
+					else if (valor == '#') PORTD  = eeprom_read_byte((uint8_t *)(15));
+					val = valor;
+			}
+		}	
+	}
 }
-
-}
-
-
+#endif

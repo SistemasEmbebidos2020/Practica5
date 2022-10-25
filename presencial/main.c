af11543530d914e6967b30b44255aa53bdc9fd69
SistemasEmbebidos <78398897+SistemasEmbebidos2020@users.noreply.github.com>
2022-10-25 12:15:26 -0500

    Update main.c

--- presencial/main.c
+++ presencial/main.c
@@ -1,12 +1,29 @@
 #include <Arduino.h>
 
+//Para leer posicion de la memoria eeprom
+//lo realizamos con el siguiente codigo
+
 #include "EepromReadSpace.h"
 
-int posicion = 0;
+uint8_t * posicion = 0;
 void setup(){
   Serial.begin(115200);
+  Serial.println(readeeproms(posicion));
 }
 
 void loop(){
-  Serial.println(readeeproms(posicion));
+  
+}
+
+
+//Para escribir un valor en cualquier posicion de la eeprom
+#include "eepromwrite.h"
+
+uint8_t * posicion = 0;
+int valor = 100;
+
+void setup(){
+  eepromwrites(posicion,valor);
+}
+void loop(){
 }
